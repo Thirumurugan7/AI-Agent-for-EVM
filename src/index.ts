1a80a228c8b2419cf246317cd642e4a691404168
thirumurugan7 <thiru82003@gmail.com>
2024-12-17 02:03:29 +0530

    handler is setted

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import OpenAI from "openai"
 import { createAssistant } from "./openai/createAssistant";
 import { createThread } from "./openai/createThread";
 import { createRun } from "./openai/createRun";
+import { performRun } from "./openai/performRun";
 
 
 async function main() {
@@ -19,6 +20,11 @@ console.log("step 1, creating an assistant");
     console.log("runnnig");
 
     const run = await createRun(client,thread,assistant.id)
+
+
+    const result = await performRun(run, client,thread);
+    console.log("result",result);
+
     
 }
 
